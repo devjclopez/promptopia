7a15bbbef1c05aecd928684767ccbfded91c7ad9
Dev. JCLopez <dev.jclopez@gmail.com>
2023-08-01 11:37:32 -0500

    final code

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,43 @@
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+
+
+export const GET = async (req, {params}) => {
+  try {
+     await connectToDB()
+     const prompt = await Prompt.findById(params.id).populate('creator')
+     if(!prompt) return new Response("Prompt not found", {status: 404})
+     return new Response(JSON.stringify(prompt), {status: 200})
+  } catch (error) {
+    console.log(error);
+    return new Response("Failed to fetch all prompts", {status: 500})
+
+  }
+}
+
+export const PATCH = async (req, {params}) => {
+  const {prompt, tag } = await req.json()
+  try {
+     await connectToDB()
+     const updatedPrompt = await Prompt.findByIdAndUpdate(params.id, {prompt: prompt, tag: tag})
+     if(!updatedPrompt) return new Response("Prompt not found", {status: 404})
+     return new Response(JSON.stringify(updatedPrompt), {status: 200})
+  } catch (error) {
+    console.log(error);
+    return new Response("Failed to updated prompt", {status: 500})
+
+  }
+}
+
+export const DELETE = async (req, {params}) => {
+  try {
+     await connectToDB()
+     await Prompt.findByIdAndRemove(params.id)
+     return new Response("Prompt deleted successfully", {status: 201})
+  } catch (error) {
+    console.log(error);
+    return new Response("Failed to delete prompt", {status: 500})
+
+  }
+}
+
